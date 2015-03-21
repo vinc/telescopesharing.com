17a730757db25e310588fa880fba3a28f0915de0
Vincent Ollivier <v@vinc.cc>
2015-03-22 11:00:08 +1300

    Add telescope controller

--- app/controllers/telescopes_controller.rb
+++ app/controllers/telescopes_controller.rb
@@ -1,2 +1,20 @@
 class TelescopesController < ApplicationController
+  expose(:telescopes)
+  expose(:telescope)
+
+  def create
+    if telescope.save
+      redirect_to(telescope)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if telescope.save
+      redirect_to(telescope)
+    else
+      render :edit
+    end
+  end
 end
