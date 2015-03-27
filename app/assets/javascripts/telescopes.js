4349607c19332379eb6c20c6ceb18f890b576fd0
Vincent Ollivier <v@vinc.cc>
2015-03-28 10:54:38 +1300

    Add custom form for scheduled_at field

--- app/assets/javascripts/telescopes.js
+++ app/assets/javascripts/telescopes.js
@@ -1,7 +1,7 @@
 $(document).on('ready page:load nested:fieldAdded', function() {
   var format = 'YYYY-MM-DD';
 
-  $('[type=date]').datetimepicker({
+  $('.telescope_observations_scheduled_at .input-group').datetimepicker({
     format: format,
     minDate: moment().format(format),
     maxDate: moment().add(1, 'month').format(format)
