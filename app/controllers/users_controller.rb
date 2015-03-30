e0b2b574568f6d994cb9b17beade335227b98883
Vincent Ollivier <v@vinc.cc>
2015-03-30 15:49:51 +1300

    Add users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
 class UsersController < ApplicationController
   expose(:users)
+  expose(:user)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     resources :observations
   end
 
+  resources :users, only: :show
+
   root "welcome#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
