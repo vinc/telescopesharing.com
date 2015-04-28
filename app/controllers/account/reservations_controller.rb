1c681d6d02a53e7090aa19252e15d62d3f73fa05
Vincent Ollivier <v@vinc.cc>
2015-04-28 18:15:17 +1200

    Update reservations index

--- app/controllers/account/reservations_controller.rb
+++ app/controllers/account/reservations_controller.rb
@@ -1,5 +1,7 @@
 class Account::ReservationsController < ApplicationController
   before_action :authenticate_user!
 
-  expose(:reservations) { Reservation.where(user: current_user) }
+  expose(:reservations) do
+    Reservation.where(user: current_user).desc(:scheduled_on)
+  end
 end
